defda9d26262a2e8494dcc0297ec7d9d46228078
Eric Iacutone <eric.iacutone@gmail.com>
2015-12-25 21:59:06 -0500

    Youtube script finished

--- youtube.sh
+++ youtube.sh
@@ -2,11 +2,14 @@
 
 echo "Script started"
 
+user_id=$1
+shift
+
 youtube-dl --default-search=ytsearch: \
            --restrict-filenames \
            --format=bestaudio \
            --audio-format=mp3 \
            --audio-quality=1 "$*" \
-           --output="/Users/iacutone/code/fun/playlister/Downloads/audio/%(title)s.%(ext)s"
+           --output="/Users/iacutone/code/fun/playlister/tmp/$user_id/%(title)s.%(ext)s"
 
-echo "Script finished"
+echo "youtube-dl finished"
